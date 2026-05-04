4001466cb6dbf96fc640c9d695088c058e5667ed
aiziro-96 <kiramatu5103@gmail.com>
2026-05-04 18:31:56 +0900

    README変更

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
+
 def get_db_connection():
     conn = sqlite3.connect('karaoke.db')
     conn.row_factory = sqlite3.Row
